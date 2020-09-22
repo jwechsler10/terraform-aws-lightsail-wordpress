db0438fd2c087c912532bf7e506ecd59ce187d2a
jwechsler10 <jwechsler10@gmail.com>
2020-09-21 21:58:01 -0400

    make it data

--- route53.tf
+++ route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "wordpress" {
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
   name    = "eats-tina.jw-kn.com"
   type    = "A"
   ttl     = "300"
